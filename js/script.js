2906311bcb876f836d518acc11c6df02c91cac71
stiva98 <samustiva9810@gmail.com>
2023-07-03 17:40:32 +0200

    ADD function al click

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const slides = [
+/*const slides = [
 {
     image: 'img/01.webp',
         title: 'Marvel\'s Spiderman Miles Morale',
@@ -22,4 +22,61 @@ const slides = [
     }
 ];
 
-console.log(slides);
+console.log(slides);*/
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        return {
+            count: 0,
+            slides: [
+                {
+                    image: 'img/01.webp',
+                    title: 'Marvel\'s Spiderman Miles Morale',
+                    text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+                    active: true,
+                }, {
+                    image: 'img/02.webp',
+                    title: 'Ratchet & Clank: Rift Apart',
+                    text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+                    active: false,
+                }, {
+                    image: 'img/03.webp',
+                    title: 'Fortnite',
+                    text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+                    active: false,
+                }, {
+                    image: 'img/04.webp',
+                    title: 'Stray',
+                    text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+                    active: false,
+                }, {
+                    image: 'img/05.webp',
+                    title: "Marvel's Avengers",
+                    text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
+                    active: false,
+                }
+            ]
+        }
+    },
+    methods: {
+        nextButton () {
+            if (this.count == (this.slides.length - 1)){
+                this.count = 0;
+            }else{
+                this.count++;
+            }
+            console.log(this.count)
+        },
+        
+
+        prevButton () {
+            if (this.count <= 0){
+                this.count = (this.slides.length - 1);
+            }else{
+                this.count--;
+            }
+            console.log(this.count)
+        }
+    }
+}).mount('#app');
